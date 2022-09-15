0ab8a13e12bb9b32de30e71ec66a0caf0ab1caa8
Gouravpundir <pundirgourav0@gmail.com>
2022-09-15 22:11:43 +0530

    updated

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,95 +1,90 @@
 const collegeModel = require("../models/collegeModel");
-const { find } = require("../models/internModel");
 const internModel = require("../models/internModel");
 const validator = require("validator");
 
-// =============================================================REGEX FOR NAME VALIDATION============================================================================
-function validateName($name) {
-    var nameReg = /^[A-Za-z]*$/;
-    if (!nameReg.test($name)) {
-        return false;
-    } else {
-        return true;
-    }
-}
 
-
-
-// ==============================================================ROUTE HANDLER FOR CREATE COLLEGE API=====================================================================
-
+// =========================CREATE COLLEGE API===========================//
 
 const createCollege = async function (req, res) {
-    try {
-        if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, msg: "Request Body Cant be Blank" })
-        let collegeData = req.body;
-
-        if (!collegeData.name) return res.status(400).send({ status: false, msg: "CollegeName is A Mandatory Field" });
-        if (!validateName(collegeData.name)) return res.status(400).send({ status: false, msg: "Invalid College Name" });
-        collegeData.name.toUpperCase();
-        let uniqueCollege = await collegeModel.findOne({ name: collegeData.name });
-        if (uniqueCollege) return res.status(400).send({ status: false, msg: "Please Enter A Different College Name,College Already Exists" });
-
-
-
-        if (!collegeData.fullName) return res.status(400).send({ status: false, msg: "College FullName Is A Mandatory Field" });
-
-
-
-        if (!collegeData.logoLink) return res.status(400).send({ status: false, msg: "Please Provide A College LogoLink,Its A Mandatory Field" });
-        if (!validator.isURL(collegeData.logoLink)) return res.status(400).send({ status: false, msg: "Invalid logoLink" });
-
-        let savedCollege = await collegeModel.create(collegeData);
-        return res.status(200).send({ status: true, data: savedCollege });
+  try {
+    let collegeData = req.body;
+    if (Object.keys(collegeData).length == 0){
+      return res
+        .status(400)
+        .send({ status: false, msg: "Please provide sufficent details in request body" });
     }
-    catch (error) {
-        return res.status(500).send({ status: false, msg: "Internal Server Error" })
-    }
-}
+    if (!collegeData.name)
+      return res
+        .status(400)
+        .send({ status: false, msg: "CollegeName is A Mandatory Field" });
+    let uniqueCollege = await collegeModel.findOne({ name: collegeData.name });
+    if (uniqueCollege)
+      return res.status(400).send({
+        status: false,
+        msg: "Please Enter A Different College Name,College Already Exists",
+      });
 
+    if (!collegeData.fullName)
+      return res
+        .status(400)
+        .send({ status: false, msg: "College FullName Is A Mandatory Field" });
 
-// ========================================================ROUTE HANDLER FOR GET COLLEGE API========================================================================
+    if (!collegeData.logoLink)
+      return res.status(400).send({
+        status: false,
+        msg: "Please Provide A College LogoLink,Its A Mandatory Field",
+      });
+    if (!validator.isURL(collegeData.logoLink))
+      return res.status(400).send({ status: false, msg: "Invalid logoLink" });
 
-
-const getCollegeDetails = async function (req, res) {
-    try {
-        let college = req.query.collegeName;
-        if (!college) return res.status(400).send({ status: false, msg: "Please Provide Query Filter" });
-        if (!validateName(college)) return res.status(400).send({ status: false, msg: "Invalid College Name" });
-        let college1 = college.toUpperCase();
-
-        req.query.isDeleted = false;
-        let collegeDetails = await collegeModel.findOne({ name: college1 }).select({ name: 1, fullName: 1, logoLink: 1 });
-        if (!collegeDetails) return res.status(404).send({ status: false, msg: "College Not Found" });
-        let collegeid = collegeDetails._id;
-
-
-        let internlist = await internModel.find({ collegeId: collegeid }).select({ name: 1, email: 1, mobile: 1 });
-        if (internlist.length == 0) {
-            internlist = "No Interns Have Applied For this College!!"
-        }
-
-        let result = {
-            name: collegeDetails.name,
-            fullName: collegeDetails.fullName,
-            logoLink: collegeDetails.logoLink,
-            interns: internlist
-        }
-        return res.status(200).send({ status: true, data: result });
-    }
-    catch (error) {
-        return res.status(500).send({ status: false, msg: "Internal Server Error" })
-    }
+    let savedCollege = await collegeModel.create(collegeData);
+    return res.status(200).send({ status: true, data: savedCollege });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ status: false, error:error.message });
+  }
 };
 
+//========================ROUTE HANDLER FOR GET COLLEGE API==============================//
 
-
-
-
-
-
-
-
-
+const getCollegeDetails = async function (req, res) {
+  try {
+    let collegeData = req.query.collegeName;
+    if (!collegeData) {
+      res
+        .status(400)
+        .send({ staus: false, msg: "please provide collage name" });
+      return;
+    }
+    let getCollegeData = await collegeModel
+      .findOne({ isDeleted: false, name: collegeData })
+      .select({name:1,fullName: 1, logoLink: 1 });
+    if (!getCollegeData) {
+      res.status(400).send({ status: false, msg: "not valid collage data" });
+      return;
+    }
+    let data = getCollegeData._id;
+    let getInterData = await internModel
+      .find({ collegeId: data })
+      .select({ name: 1, email: 1, mobile: 1 });
+      if(getInterData.length==0){
+       getInterData="please provide intern details"
+      }
+    let result = {
+      _id: getCollegeData._id,
+      name: getCollegeData.name,
+      fullName: getCollegeData.fullName,
+      logoLink: getCollegeData.logoLink,
+      interns: getInterData,
+    };
+    return res.status(200).send({ status: true, data: result });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ status: false, error:error.message});
+  }
+};
 
 module.exports.createCollege = createCollege;
 module.exports.getCollegeDetails = getCollegeDetails;
--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -1,86 +1,75 @@
 const internModel = require("../models/internModel");
 const collegeModel = require("../models/collegeModel");
-const validator = require("validator");
 
-// ===============================================Regex For Validations================================================================================================
-function validateName($name) {
-    var nameReg = /^[A-Za-z ]*$/;
-    if (!nameReg.test($name)) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-
-function validateMobile($mobile) {
-    var mobileReg = /^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/;
-    if (!mobileReg.test($mobile)) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function ConversionToProperName(name) {
-
-    let name2 = name.toLowerCase().split(" ");
-    return name2.map((x) => { return x[0].toUpperCase() + x.substring(1) }).join(" ");
-
-}
-
-
-
-// ================================================================ROUTE HANDLER FOR CREATE INTERN API====================================================================================
+// ========================ROUTE HANDLER FOR CREATE INTERN API====================//
 
 const createIntern = async function (req, res) {
-    try {
-        if (!req.body) return res.status(400).send({ status: false, msg: "Request Body Cant be Blank" })
-        let internData = req.body;
-
-
-        if (!internData.name) return res.status(400).send({ status: false, msg: "Name is A Mandatory Field,Please Input Name" });
-        if (!validateName(internData.name)) return res.status(400).send({ status: false, msg: "InValid Name" });
-        internData.name = ConversionToProperName(internData.name);
-
-
-
-        if (!internData.email) return res.status(400).send({ status: false, msg: "Email is A Mandatory Field,Please Input Email" });
-        if (!validator.isEmail(internData.email)) return res.status(400).send({ status: false, msg: "Invalid Email" });
-
-        if (!internData.mobile) return res.status(400).send({ status: false, msg: "Mobile is A Mandatory Field,Please Input Mobile" });
-        if (!validateMobile(internData.mobile)) return res.status(400).send({ status: false, msg: "InValid Mobile" });
-
-
-        let uniqueMobileEmail = await internModel.find({ $or: [{ email: internData.email }, { mobile: internData.mobile }] });
-        if (uniqueMobileEmail.length !== 0) {
-            if (uniqueMobileEmail[0].email == internData.email) return res.status(400).send({ status: false, msg: "EmailId Already Exists,Please Input Another EmailId" })
-            else { return res.status(400).send({ status: false, msg: "Mobile Number Already Exists,Please Input Another Mobile Number" }) }
-        };
-
-        let college = await collegeModel.findOne({ name: internData.collegeName, isDeleted: false });
-        if (!college) return res.status(400).send({ status: false, msg: "No Such College Found,Please Enter A Valid College Name" })
-        let id = college._id;
-
-
-        let obj = {
-            name : internData.name,
-            email : internData.email,
-            mobile : internData.mobile,
-            collegeId : id
-
-        }
-       
-
-        let savedIntern = await internModel.create(obj);
-        return res.status(201).send({ status: true, data: savedIntern });
+  try {
+    let internData = req.body;
+    if (Object.keys(internData).length == 0) {
+      return res
+        .status(400)
+        .send({
+          status: false,
+          msg: "Please provide sufficent details in request body",
+        });
     }
-    catch (error) {
-        return res.status(500).send({ status: false, msg: "Internal Server Error" })
+    let {name,email,mobile}=internData
+    if (!name)
+      return res.status(400).send({
+        status: false,
+        msg: "Name is A Mandatory Field,Please Input Name",
+      });
+
+    if (!email)
+      return res.status(400).send({
+        status: false,
+        msg: "Email is A Mandatory Field,Please Input Email",
+      });
+
+    const isEmailAlreadyUsed = await internModel.findOne({ email });
+    if (isEmailAlreadyUsed) {
+      res.status(400).send({
+        status: false,
+        message: `${email} email is already registered`,
+      });
+      return;
     }
+
+    const isMobileAlreadyUsed = await internModel.findOne({ mobile });
+    if (isMobileAlreadyUsed) {
+      res.status(400).send({
+        status: false,
+        message: `${mobile} mobile is already registered`,
+      });
+      return;
+    }
+
+    let college = await collegeModel.findOne({
+      name: internData.collegeName,
+      isDeleted: false,
+    });
+    if (!college)
+      return res.status(400).send({
+        status: false,
+        msg: "No Such College Found,Please Enter A Valid College Name",
+      });
+    let id = college._id;
+
+    let obj = {
+      name: internData.name,
+      email: internData.email,
+      mobile: internData.mobile,
+      collegeId: id,
+    };
+
+    let internDataCreated = await internModel.create(obj);
+    return res.status(201).send({ status: true, data: internDataCreated  });
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ status: false, error:error.message });
+  }
 };
 
-
 module.exports.createIntern = createIntern;
-
-
